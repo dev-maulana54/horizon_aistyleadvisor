788496384a4abdbb06944a7303b09eddbb7bbad9
Maulana <lanayeager12@gmail.com>
2026-03-12 05:36:01 +0700

    ini tambah si tb

--- app/Controllers/Settings.php
+++ app/Controllers/Settings.php
@@ -8,10 +8,10 @@ class Settings extends BaseController
     public function __construct()
     {
         // Cek apakah session isLoggedIn tidak ada atau tidak bernilai true, maka redirect ke login
-        if (!session()->get('isLoggedIn') || session()->get('isLoggedIn') !== true) {
-            header('Location: ' . base_url('user/login'));
-            exit(); // ✅ WAJIB pakai exit() agar script berhenti
-        }
+        // if (!session()->get('isLoggedIn') || session()->get('isLoggedIn') !== true) {
+        //     header('Location: ' . base_url('user/login'));
+        //     exit(); // ✅ WAJIB pakai exit() agar script berhenti
+        // }
     }
     public function index(): string
     {
