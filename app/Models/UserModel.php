94f0f0fc79f419546b22fcc1c36add71804abd4d
Maulana <lanayeager12@gmail.com>
2026-03-13 04:25:14 +0700

    Login Register Auth

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -9,6 +9,18 @@ class UserModel extends Model
     protected $table            = 'users';
     protected $primaryKey       = 'id';
     protected $returnType       = 'array';
-    protected $allowedFields    = ['name', 'email', 'password'];
+    protected $allowedFields    = ['nama', 'email', 'password', 'role', 'is_active', 'is_premium'];
     protected $useTimestamps    = true;
+
+    // Query Builder untuk mencari user berdasarkan email
+    public function getUserByEmail($email)
+    {
+        return $this->where('email', $email)->first();
+    }
+
+    // Query Builder untuk registrasi user baru
+    public function registerUser($data)
+    {
+        return $this->insert($data);
+    }
 }
